ff020f75cebca4c7d1ef63bd977f37daf3646abf
ArfanaSuraibaR <76675562+ArfanaSuraibaR@users.noreply.github.com>
2021-02-23 18:56:46 +0530

    Update Employee Sort Name.java

--- Employee Sort Name.java
+++ Employee Sort Name.java
@@ -1,1 +1,18 @@
 package emp;
+public class EmpName{
+  private Name;
+  private Dept;
+}
+public EmpName(String Name,String Dept)
+{
+  this.Name=Name;
+  this.Dept=Dept;
+}
+public String getName()
+{
+  return getName();
+}
+public String getDept()
+{
+  return getDept();
+}
