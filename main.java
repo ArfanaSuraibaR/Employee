95d4b2938fa84ced41414030a78ff55a0334474f
ArfanaSuraibaR <76675562+ArfanaSuraibaR@users.noreply.github.com>
2021-02-23 18:50:54 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
-package
+package emp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Main{
+  public static void main(String args[])
+  {
+    list<Emp> array=new ArrayList<>();
+    array.add(new Emp("Marry","QEA"));
+    array.add(new Emp("Jose","ADM"));
+    array.add(new Emp("vicky","CIS"));
+    array.add(new Emmp("Aris","SAP"));
+    
+    Collections.sort(arr,new sortname());
+    for(Emp e:arr)
+    {
+      System.out.println("Name:"+getEmpName());
+      System.out.println("DepartmentId:"+getDeptId());}
+  }
+}
+    
